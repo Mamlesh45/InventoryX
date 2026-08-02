334b5c122d15b2cfd793a7bda3bdaf66050e3425
Mamlesh Yerge <mamleshwaryerge2@gmail.com>
2026-08-02 12:22:13 +0530

    Initial commit - InventoryX project setup

---
+++ src/main/java/com/inventoryx/InventoryxApplication.java
@@ -0,0 +1,13 @@
+package com.inventoryx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryxApplication.class, args);
+	}
+
+}
