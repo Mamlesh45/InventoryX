2641ad718de174889781864ef45a200f0a533e29
Mamlesh Yerge <mamleshwaryerge2@gmail.com>
2026-08-04 15:00:38 +0530

    Phase 4 - Complete Product CRUD API

--- src/main/java/com/inventoryx/controller/ProductController.java
+++ src/main/java/com/inventoryx/controller/ProductController.java
@@ -1,6 +1,12 @@
 package com.inventoryx.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +29,25 @@ public class ProductController {
 	@PostMapping
 	public Product saveProduct(@RequestBody Product product) {
 		return productService.saveProduct(product);
-		
+	}
+	
+	@GetMapping
+	public List<Product> getAllProducts(){
+		return productService.getAllProducts();
+	}
+	
+	@GetMapping("/{id}")
+	public Product getProductById(@PathVariable Long id) {
+		return productService.getProductById(id);
+	}
+	
+	@PutMapping("/{id}")
+	public Product updateProduct(@PathVariable Long id , @RequestBody Product product) {
+		return productService.updateProduct(id, product);
+	}
+	
+	@DeleteMapping("/{id}")
+	public String deleteProduct(@PathVariable Long id) {
+		return productService.deleteProduct(id);
 	}
 }
--- src/main/java/com/inventoryx/service/ProductService.java
+++ src/main/java/com/inventoryx/service/ProductService.java
@@ -1,5 +1,7 @@
 package com.inventoryx.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.inventoryx.entity.Product;
@@ -15,13 +17,42 @@ public class ProductService {
 	}
 	
 	public Product saveProduct(Product product) {
-
-	    System.out.println(product.getName());
-	    System.out.println(product.getDescription());
-	    System.out.println(product.getSku());
-	    System.out.println(product.getPrice());
-	    System.out.println(product.getQuantity());
-
 	    return productRepository.save(product);
 	}
+	
+	public List<Product> getAllProducts(){
+		return productRepository.findAll();
+	}
+	
+	public Product getProductById(Long id) {
+		return productRepository.findById(id).orElse(null);
+	}
+	
+	
+	public Product updateProduct(Long id , Product updatedProduct) {
+		Product product = productRepository.findById(id).orElse(null);
+		if(product == null) {
+			return null;
+		}
+		product.setName(updatedProduct.getName());
+		product.setDescription(updatedProduct.getDescription());
+		product.setSku(updatedProduct.getSku());
+		product.setPrice(updatedProduct.getPrice());
+		product.setQuantity(updatedProduct.getQuantity());
+		
+		return productRepository.save(product);
+	}
+	
+	
+	public String deleteProduct(Long id) {
+		Product product = productRepository.findById(id).orElse(null);
+		
+		if(product == null) {
+			return "Product Not Found";
+		}
+		productRepository.delete(product);
+		
+		return "Product Deleted Successfully";
+	}
+	
 }
